5401177de91abe61cae83db94db483c9000ebf10
Rimakhrkhryan <rimakhrkhryan@gmail.com>
2024-08-13 10:35:06 +0400

    Update DeepCloneRunner.java

--- DeepCloneRunner.java
+++ DeepCloneRunner.java
@@ -10,7 +10,10 @@ import java.util.Map;
 
 public class DeepCloneRunner {
 	public static void main(String[] args) {
-		var books = Arrays.asList("book1","book2");
+		var books =new ArrayList<String>();
+		books.add("book1");
+		books.add("book2");
+		var man = new Man("Name", 25, books);
 		var man = new Man("Name",25,books);
 		var manCopy = DeepCloneHelper.deepClone(man);
 		System.out.println(manCopy == man);
